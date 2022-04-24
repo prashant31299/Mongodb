d72ad2e3454d71c1dafbc3a0c6fc572bece13338
prashantchavan <prashantchavan312199@gmail.com>
2022-04-24 14:39:43 +0530

    add mern mongo

---
+++ backend/config/db.js
@@ -0,0 +1,29 @@
+
+const {MongoClient} = require('mongodb');
+// const {MongoClient} = require('mongodb');
+
+async function connectdb(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://prashant:9KezRx68whx8mvjO@Cluster0/test?retryWrites=true&w=majority";
+ 
+
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+module.exports=connectdb
--- backend/server/server.js
+++ backend/server/server.js
@@ -1,9 +1,19 @@
 const express =require('express')
-const notes = require('../../frontend/src/data/notes')
+const notes = require('../data/notes')
 const dotenv= require('dotenv')
-
+const mongoose=require('mongoose')
+const connectdb= require('../config/db')
 const app =express()
 dotenv.config()
+connectdb()
+
+const DB='mongodb+srv://prashant:Madhav@@007@cluster0.mm7yr.mongodb.net/mern_data?retryWrites=true&w=majority'
+
+mongoose.connect(DB).then(()=>{
+    console.log("connectioon success");
+}).catch((err)=>{
+    console.log(`Error connection Failed ${err}`);
+})
 
 app.get('/', (req,res)=>{
     res.send("Api is running ")
